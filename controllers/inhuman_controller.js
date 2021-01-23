c973ae88591a609ba072b24368e7f25010262995
kaylamuraoka <kmurs98@gmail.com>
2021-01-23 08:17:15 -1000

    setting up CRUD functionality and the handlebars to render the data in the browser

--- controllers/inhuman_controller.js
+++ controllers/inhuman_controller.js
@@ -1,24 +1,40 @@
 const db = require("../models");
 
 module.exports = (app) => {
-  // GET route for getting all of the todos
-  app.get("/api/todos", (req, res) => {
-    // Find all todos, and return them to the user with res.json
-    db.Todo.findAll({}).then((inhumans_test_db) => {
-      res.json(inhumans_test_db);
+  // GET route for getting all of the actors
+  app.get("/api/actors", (req, res) => {
+    // Find all actors, and return them to the user with res.json
+    db.Actor.findAll({}).then((inhumans_test_db) => {
+      let actorsArray = [];
+      for (let i = 0; i < inhumans_test_db.length; i++) {
+        let actor = {
+          id: inhumans_test_db[i].id,
+          firstName: inhumans_test_db[i].firstName,
+          lastName: inhumans_test_db[i].lastName,
+        };
+        actorsArray.push(actor);
+      }
+      console.log("Array of actors", actorsArray);
+
+      res.render("partials/actors/actors-block", {
+        actors: actorsArray,
+      });
+
+      // res.json(inhumans_test_db);
     });
   });
+
   // POST route for saving a new todo. You can create a todo using the data on req.body
-  app.post("/api/todos", (req, res) => {
-    db.Todo.create({
-      text: req.body.text,
-      complete: req.body.complete,
-    }).then((dbTodo) => res.json(dbTodo));
+  app.post("/api/actors", (req, res) => {
+    db.Actor.create({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+    }).then((inhumans_test_db) => res.json(inhumans_test_db));
   });
 
   // DELETE route for deleting todos. You can access the todo's id in req.params.id
-  app.delete("/api/todos/:id", (req, res) => {
-    db.Todo.destroy({
+  app.delete("/api/actors/:id", (req, res) => {
+    db.Actor.destroy({
       where: {
         id: req.params.id,
       },
@@ -28,11 +44,11 @@ module.exports = (app) => {
   });
 
   // PUT route for updating todos. The updated todo will be available in req.body
-  app.put("/api/todos", (req, res) => {
-    db.Todo.update(
+  app.put("/api/actors", (req, res) => {
+    db.Actor.update(
       {
-        text: req.body.text,
-        complete: req.body.complete,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
       },
       {
         where: {
--- models/inhuman.js
+++ models/inhuman.js
@@ -1,19 +1,28 @@
 module.exports = (sequelize, DataTypes) => {
-  const Todo = sequelize.define("Todo", {
-    text: {
+  const { Sequelize } = sequelize;
+  const Actor = sequelize.define("Actor", {
+    firstName: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        len: [1],
+        len: [1, 40],
       },
     },
-    complete: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: true,
       validate: {
-        len: [1],
+        len: [1, 40],
       },
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: Sequelize.literal("now()"),
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      defaultValue: Sequelize.literal("now()"),
     },
   });
-  return Todo;
+  return Actor;
 };
