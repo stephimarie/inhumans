5dc6242bf8f75a54d6cb97c3cc40620762779adc
stvgo15 <stv.gore@gmail.com>
2021-01-25 17:06:29 -0800

    working on the funtions and then i work in the style, but this is how far I'm going

---
+++ public/js/chartSelect.js
@@ -0,0 +1,25 @@
+// $('#ChartButtons').on('click', 'button', function(){
+//     console.log("helloworld")
+// })
+
+// $("#ChartButtons").click(function() {
+//     $("#ChartButtons").css("background-color", "white");
+//     $(this).css("background-color", "red");
+// });
+
+$("#Chart1").click(function(){
+    $(".background").css("background", "url(../assets/Medusaback.jpg) center fixed")
+})
+
+$("#Chart2").click(function(){
+    $(".background").css("background", "url(../assets/Blackboltback.jpg) center fixed")
+})
+
+$("#Chart3").click(function(){
+    $(".background").css("background", "url(../assets/Crystalback.jpg) center fixed")
+})
+
+$("#Chart4").click(function(){
+    $(".background").css("background", "url(../assets/Tritonback.jpg) center fixed")
+})
+
