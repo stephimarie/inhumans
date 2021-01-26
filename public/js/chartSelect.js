f72ca9528e8aad1a4c679b0ccf495b6018fbe2e0
stvgo15 <stv.gore@gmail.com>
2021-01-26 00:59:00 -0800

    front-end1

--- public/js/chartSelect.js
+++ public/js/chartSelect.js
@@ -7,19 +7,40 @@
 //     $(this).css("background-color", "red");
 // });
 
+const target = window.document.getElementsByTagName('h1')[0]
+
+const flickerLetter = letter => `<span style="animation: text-flicker-in-glow ${Math.random()*4}s linear both ">${letter}</span>`
+const colorLetter = letter => `<span style="color: hsla(${Math.random()*360}, 100%, 80%, 1);">${letter}</span>`;
+const flickerAndColorText = text => 
+  text
+    .split('')
+    .map(flickerLetter)
+    .map(colorLetter)
+    .join('');
+const neonGlory = target => target.innerHTML = flickerAndColorText(target.textContent);
+
+
+neonGlory(target);
+target.onclick = ({ target }) =>  neonGlory(target);
+
 $("#Chart1").click(function(){
-    $(".background").css("background", "url(../assets/Medusaback.jpg) center fixed")
+    $(".background").css("background", "url(../assets/Background2.jpeg)")
 })
 
 $("#Chart2").click(function(){
-    $(".background").css("background", "url(../assets/Blackboltback.jpg) center fixed")
+    $(".background").css("background", "url(../assets/Background1.jpeg) center fixed")
 })
 
 $("#Chart3").click(function(){
-    $(".background").css("background", "url(../assets/Crystalback.jpg) center fixed")
+    $(".background").css("background", "url(../assets/Background3.jpeg) center fixed")
 })
 
 $("#Chart4").click(function(){
-    $(".background").css("background", "url(../assets/Tritonback.jpg) center fixed")
+    $(".background").css("background", "url(../assets/Background4.jpeg) center fixed")
 })
+
+$("#Chart5").click(function(){
+    $(".background").css("background", "url(../assets/Background88.jpeg) center fixed")
+})
+
 
